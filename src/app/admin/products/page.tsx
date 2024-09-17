e7b70a1d46f3bb341a9f1a0fceb76b33f4811261
Abhivera <abhijitverma2016@gmail.com>
2024-09-18 01:15:19 +0530

    1:10 Hours

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -2,43 +2,82 @@ import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle, CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 export default function AdminProductsPage() {
   return (
     <>
-    <div className="flex justify-between items-center gap-4">
-      <PageHeader>Products</PageHeader>
-      <Button>
-        <Link href="/admin/products/new">Add Product</Link>
-      </Button>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
       </div>
-      <ProductsTable/>
+      <ProductsTable />
     </>
   );
 }
-function ProductsTable(){
-    return <Table>
-        <TableHeader>
-            <TableRow>
-            <TableHead className="w-0">
-                    <span className="sr-only">Available for Purchase</span>
-                </TableHead>
-                <TableHead>Name</TableHead>
-                <TableHead>Price</TableHead>
-                <TableHead>Order</TableHead>
-                <TableHead className="w-0">
-                    <span className="sr-only">Actions</span>
-                </TableHead>
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-
-        </TableBody>
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+  });
+  if (products.length === 0) return <p>No products found</p>;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Order</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <span className="sr-only">Available</span>
+                  <CheckCircle2 />
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Not Available</span>
+                  <XCircle />
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+      
+            <TableCell>
+              <MoreVertical />
+              <span className="sr-only">Actions</span>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
+  );
 }
